420c813895188904a72a84863233f5f0bde7831d
Anthony Albertini <kiki@MacBook-Pro-de-Anthony.local>
2014-08-30 01:06:53 +0200

    test(TryTest.spec.js): add sample test

---
+++ tests/js/spec/views/TryTest.spec.js
@@ -0,0 +1,15 @@
+define([],
+
+function () {
+  'use strict';
+
+  describe('Try test', function () {
+
+    describe('structure', function () {
+
+      it('should implement some tests', function () {
+        true.should.be.true;
+      });
+    });
+  });
+});
