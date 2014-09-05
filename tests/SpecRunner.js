d8671f150144ca17d3a3d8666943ad676c1a88a0
Anthony Albertini <kiki@MacBook-Pro-de-Anthony.local>
2014-09-06 00:38:44 +0200

    test(SpecRunner.js): update SpecRunner.js

--- tests/SpecRunner.js
+++ tests/SpecRunner.js
@@ -1,31 +1,33 @@
 require.config({
-  baseUrl: '../app',
+  baseUrl: '../app/scripts',
   paths: {
-    jquery: 'bower_components/jquery/dist/jquery',
-    underscore: 'bower_components/underscore/underscore',
-    backbone: 'bower_components/backbone/backbone',
-    marionette: 'bower_components/backbone.marionette/lib/core/backbone.marionette',
-    'backbone.babysitter': 'bower_components/backbone.babysitter/lib/backbone.babysitter',
-    'backbone.wreqr': 'bower_components/backbone.wreqr/lib/backbone.wreqr',
-    handlebars: 'scripts/vendor/handlebars',
-    text: 'bower_components/requirejs-text/text',
-    json: 'bower_components/requirejs-plugins/src/json',
-    replace: 'bower_components/require.replace/require.replace',
-    chai: 'bower_components/chai/chai',
-    'chai-jquery': 'bower_components/chai-jquery/chai-jquery',
-    sinonjs: 'bower_components/sinonjs/sinon',
-    'sinon-chai': 'bower_components/sinon-chai/lib/sinon-chai'
+    jquery: '../bower_components/jquery/dist/jquery',
+    underscore: '../bower_components/lodash/dist/lodash',
+    backbone: '../bower_components/backbone/backbone',
+    marionette: '../bower_components/backbone.marionette/lib/core/backbone.marionette',
+    'backbone.babysitter': '../bower_components/backbone.babysitter/lib/backbone.babysitter',
+    'backbone.wreqr': '../bower_components/backbone.wreqr/lib/backbone.wreqr',
+    handlebars: '../bower_components/require-handlebars-plugin/Handlebars',
+    hbs: '../bower_components/requirejs-hbs/hbs',
+    text: '../bower_components/requirejs-text/text',
+    tmpl: "../templates",
+    json: '../bower_components/requirejs-plugins/src/json',
+    tests: '../../tests',
+    chai: '../bower_components/chai/chai',
+    'chai-jquery': '../bower_components/chai-jquery/chai-jquery',
+    sinonjs: '../bower_components/sinonjs/sinon',
+    'sinon-chai': '../bower_components/sinon-chai/lib/sinon-chai'
   },
   deps: ['jquery', 'underscore'],
   shim: {
     jquery: {
       exports: '$'
     },
+    underscore: {
+      exports: '_'
+    },
     backbone: {
       deps: ['jquery', 'underscore']
-    },
-    marionette: {
-      deps: ['backbone']
     },
     chai: {
       deps: ['jquery'],
@@ -55,7 +57,7 @@ function (require, Marionette, chai, chaiJquery, sinon, sinonChai) {
   chai.use(sinonChai);
 
   require([
-    '../tests/js/spec/views/TryTest.spec'
+    'tests/js/spec/views/helloWorldItemView.spec'
   ], function () {
     mocha.run();
   })
