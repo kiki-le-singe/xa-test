2074fe1a5b804a1b513e4eb81b2177c179e39388
Anthony Albertini <kiki@MacBook-Pro-de-Anthony.local>
2014-09-12 21:35:58 +0200

    feat(server.test): add server for serve tests in browser
    Only used for debugging. Do not use it in prod

---
+++ server/test.js
@@ -0,0 +1,29 @@
+// server/test.js
+
+////////////////
+// BASE SETUP //
+////////////////
+
+// Module dependencies.
+var application_root = __dirname,
+    express = require('express'), // Web framework
+    app = express(), // define server
+    path = require('path'), // Utilities for dealing with file paths
+    bodyParser = require('body-parser'),
+    port = process.env.PORT || 9001; // set our port
+
+// parses request body and populates request.body
+app.use(bodyParser());
+// where to serve static content
+app.use(express.static(path.join(application_root, '../')));
+
+
+//////////////////////
+// START THE SERVER //
+//////////////////////
+
+app.listen(port, function() {
+  console.log('Express server listening on port %d in %s node', port, app.settings.env);
+});
+
+
