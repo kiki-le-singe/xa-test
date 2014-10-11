bfa7fec3b5905d202539e30278bcb58ab2041862
Anthony Albertini <kiki@tv-lg-salon.home>
2014-10-11 22:27:04 +0200

    feat(helpers): use bar and foo helpers for example

--- app/scripts/helpers/helpers.js
+++ app/scripts/helpers/helpers.js
@@ -5,19 +5,14 @@ define([
 function (Handlebars) {
   'use strict';
 
-  // Good idea ???
-  var myObject = Object.create(null);
-
-  myObject.bar = function (context, options) {
-    return context + "!!!!!!!!";
+  var bar = function (context, options) {
+    return context;
   };
 
-  myObject.foo = function (context, options) {
-    return context + "!!!!!!!!";
+  var foo = function (context, options) {
+    return context;
   };
 
-  Handlebars.registerHelper('bar', myObject.bar);
-  Handlebars.registerHelper('foo', myObject.foo);
-
-  return myObject;
+  Handlebars.registerHelper('bar', bar);
+  Handlebars.registerHelper('foo', foo);
 });
