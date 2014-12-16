8ea4a1503c3d28b03b207fc9d6b328afc27df45b
Anthony Albertini <kiki@MacBook-Pro-de-Anthony.local>
2014-12-16 23:16:34 +0100

    test(linter): fix linter

--- tests/js/spec/controllers/nav.spec.js
+++ tests/js/spec/controllers/nav.spec.js
@@ -15,83 +15,84 @@ function (Backbone, Marionette, NavController, LibrariesCollection) {
     var $div = $('div');
     $div.attr({id: 'content'});
 
-	var librariesMock = [
-      { 
-        name : 'HTML5 Boilerplate',
-        url : 'http://www.w3.org/TR/html5/'
+    var librariesMock = [
+      {
+        name: 'HTML5 Boilerplate',
+        url: 'http://www.w3.org/TR/html5/'
       },
       {
-        name : 'jQuery',
-        url : 'http://jquery.com/'
+        name: 'jQuery',
+        url: 'http://jquery.com/'
       },
       {
-        name : 'Backbone.js',
-        url : 'http://backbonejs.org/'
+        name: 'Backbone.js',
+        url: 'http://backbonejs.org/'
       }
-	];
+    ];
 
-	before(function () {
-		this.collection = new LibrariesCollection(librariesMock);
-	});
+    before(function () {
+      this.collection = new LibrariesCollection(librariesMock);
+    });
 
-	beforeEach(function () {
-		$('body').append($div);
+    beforeEach(function () {
+      $('body').append($div);
 
-		var region = new Marionette.Region({
-			el: '#content'
-		});
-		this.controller = new NavController({contentRegion: region});
-	});
+      var region = new Marionette.Region({
+        el: '#content'
+      });
+      this.controller = new NavController({contentRegion: region});
+    });
 
-	afterEach(function () {
-		this.controller.destroy();
-		this.controller = null;
+    afterEach(function () {
+      this.controller.destroy();
+      this.controller = null;
 
-      	$div.remove();
-	});
+      $div.remove();
+    });
 
     describe('render', function () {
-	    describe('home()', function () {
-			it("should render the home view", sinon.test(function () {
-				var view = this.controller.home().currentView;
-				var $el = view.$el;
+      describe('home()', function () {
+        it('should render the home view', sinon.test(function () {
+          var view = this.controller.home().currentView;
+          var $el = view.$el;
 
-				$el.should
-					.exist.and // https://github.com/chaijs/chai-jquery#exist
-					.have.class('hello-world'); // https://github.com/chaijs/chai-jquery#classclassname
-			}));
+          $el.should
+            .exist.and // https://github.com/chaijs/chai-jquery#exist
+            .have.class('hello-world'); // https://github.com/chaijs/chai-jquery#classclassname
+        }));
 
-			it("should contain some texts", sinon.test(function () {
-				var view = this.controller.home().currentView;
-				var $el = view.$el;
+        it('should contain some texts', sinon.test(function () {
+          var view = this.controller.home().currentView;
+          var $el = view.$el;
 
-				$el.should.contain('CONGRATS!');
-				$el.should.contain('Welcome in my "Marionette Boilerplate" project !');
-				$el.should.contain('Play with this boilerplate and... Have fun!');
-				$el.should.contain("Enjoy coding!");
-			}));
-	    });
+          $el.should.contain('CONGRATS!');
+          $el.should.contain('Welcome in my "Marionette Boilerplate" project !');
+          $el.should.contain('Play with this boilerplate and... Have fun!');
+          $el.should.contain('Enjoy coding!');
+        }));
+      });
 
-	    describe('libraries()', function () {
-			it("should render the libraries view", sinon.test(function () {
-				var view = this.controller.libraries().currentView;
-				var $el = view.$el;
+      describe('libraries()', function () {
+        it('should render the libraries view', sinon.test(function () {
+          var view = this.controller.libraries().currentView;
+          var $el = view.$el;
 
-				$el.should.exist; // https://github.com/chaijs/chai-jquery#exist
-				$el.should.have('ul.libraries'); // https://github.com/chaijs/chai-jquery#haveselector
-			}));
+          /* jshint -W030 */
+          $el.should.exist; // https://github.com/chaijs/chai-jquery#exist
+          $el.should.have('ul.libraries'); // https://github.com/chaijs/chai-jquery#haveselector
+        }));
 
-			it("should render the libraries item view", sinon.test(function () {
-				var view = this.controller.libraries().currentView;
-				var $el = view.$el;
-				// sets the collection of the view with the fake collection
-				view.collection = this.collection;
-				// and rerender the view (because it was rendered a first time by the region)
-				view.render();
+        it('should render the libraries item view', sinon.test(function () {
+          var view = this.controller.libraries().currentView;
+          var $el = view.$el;
+          // sets the collection of the view with the fake collection
+          view.collection = this.collection;
+          // and rerender the view (because it was rendered a first time by the region)
+          view.render();
 
-				$el.should.have('ul.libraries li.library'); // https://github.com/chaijs/chai-jquery#haveselector
-			}));
-	    });
+          $el.should.have('ul.libraries li.library'); // https://github.com/chaijs/chai-jquery#haveselector
+        }));
+      });
     });
   });
 });
