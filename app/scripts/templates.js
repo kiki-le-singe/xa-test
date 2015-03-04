64cf340a9b82042ec7ae77680b1d3918097fa0b9
kiki le singe <aalbertini95@gmail.com>
2015-03-04 23:34:29 +0100

    feat(rootLayoutView): add rootLayout view

--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -1,6 +1,7 @@
 define(function (require) {
   'use strict';
   return {
+    'root-layout': require('hbs!tmpl/root-layout'),
     menu: require('hbs!tmpl/menu'),
     'hello-world': require('hbs!tmpl/hello-world'),
     libraries: require('hbs!tmpl/libraries'),
---
+++ app/scripts/views/rootLayoutView.js
@@ -0,0 +1,23 @@
+define([
+  'marionette',
+  'templates'
+],
+
+function (Marionette, templates) {
+  'use strict';
+
+  return Marionette.LayoutView.extend({
+    el: 'body',
+    template: templates['root-layout'],
+
+    regions: {
+      header: '#header',
+      content: '#content',
+      footer: '#footer'
+    },
+
+    initialize: function () {
+      console.log('initialize: rootLayoutView');
+    }
+  });
+});
