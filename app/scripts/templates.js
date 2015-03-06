8f8f995d8af198e7961b5babd041e5dce3fae7bc
Anthony Albertini <aalbertini@clever-age.com>
2015-03-06 17:05:23 +0100

    refactor(templates): update template path

--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -1,10 +1,10 @@
 define(function (require) {
   'use strict';
   return {
-    'root-layout': require('hbs!tmpl/root-layout'),
-    menu: require('hbs!tmpl/menu'),
-    'hello-world': require('hbs!tmpl/hello-world'),
-    libraries: require('hbs!tmpl/libraries'),
-    library: require('hbs!tmpl/library')
+    'root-layout': require('hbs!templates/root-layout'),
+    menu: require('hbs!templates/menu'),
+    'hello-world': require('hbs!templates/hello-world'),
+    libraries: require('hbs!templates/libraries'),
+    library: require('hbs!templates/library')
   };
 });
