57252ade4f9e3b8c2ff1cbbf6ac8e8e9979d9a78
Anthony Albertini <aalbertini@clever-age.com>
2015-03-06 17:06:30 +0100

    refactor(handlebars): update handlebars task

--- grunt/options/handlebars.js
+++ grunt/options/handlebars.js
@@ -4,14 +4,14 @@ module.exports = {
   compile: {
     options: {
       processName: function (filePath) {
-        var matches = filePath.match(new RegExp('app\/templates\/(.*).hbs'));
-        return matches ? matches[1] : filePath;
+        var matches = filePath.match(new RegExp('app/scripts/(modules/templates|templates)/(.*).hbs'));
+        return matches ? matches[2] : filePath;
       },
       namespace: 'templates',
       amd: true
     },
     files: {
-      '<%= yeoman.tmp %>/scripts/templates.js': ['<%= yeoman.app %>/templates/**/*.hbs']
+      '<%= yeoman.tmp %>/scripts/templates.js': ['<%= yeoman.app %>/scripts/**/*.hbs']
     }
   }
 };
