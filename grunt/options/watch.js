d38216ee61c0522d06ef77f2d0c56bd559828671
Anthony Albertini <aalbertini@clever-age.com>
2015-03-11 15:42:15 +0100

    style(watch): indent code

--- grunt/options/watch.js
+++ grunt/options/watch.js
@@ -14,11 +14,11 @@ module.exports = {
   },
   livereload: {
     files: [
-    '<%= yeoman.app %>/*.html',
-    '{.tmp,<%= yeoman.app %>}/styles/{,**/}*.css',
-    '{.tmp,<%= yeoman.app %>}/scripts/{,**/}*.js',
-    '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp}',
-    // 'test/spec/{,**/}*.js' TODO livereload the next new tests
+      '<%= yeoman.app %>/*.html',
+      '{.tmp,<%= yeoman.app %>}/styles/{,**/}*.css',
+      '{.tmp,<%= yeoman.app %>}/scripts/{,**/}*.js',
+      '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp}',
+      // 'test/spec/{,**/}*.js' TODO livereload the next new tests
     ],
     options: {
       livereload: true
