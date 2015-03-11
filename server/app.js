0401b6d8252049c66b83cbb45196880abb4ea662
Anthony Albertini <aalbertini@clever-age.com>
2015-03-11 14:45:11 +0100

    style(app): fix linter

--- server/app.js
+++ server/app.js
@@ -1,63 +1,61 @@
-// server/app.js
+/***********
+ BASE SETUP
+************/
 
-////////////////
-// BASE SETUP //
-////////////////
-
-// Stubs //
+// Stubs
 var libraries = require('./stubs/libraries.json');
 
 // Module dependencies.
-var application_root = __dirname,
+var applicationRoot = __dirname,
     express = require('express'), // Web framework
     app = express(), // define server
     path = require('path'), // Utilities for dealing with file paths
-    mongoose = require('mongoose'), //MongoDB integration
+    mongoose = require('mongoose'), // MongoDB integration
     bodyParser = require('body-parser'),
     port = process.env.PORT || 9000, // set our port
     args = process.argv,
-    stubArg = (args[2] === 'true');
+    stubArg = ('true' === args[2]);
 
 // parses request body and populates request.body
 app.use(bodyParser());
 // where to serve static content
-app.use(express.static(path.join(application_root, '../app')));
-app.use(express.static(path.join(application_root, '../.tmp')));
+app.use(express.static(path.join(applicationRoot, '../app')));
+app.use(express.static(path.join(applicationRoot, '../.tmp')));
 
-////////////////////////
-// ROUTES FOR OUR API //
-////////////////////////
+/*******************
+ ROUTES FOR OUR API
+********************/
 
 var router = express.Router(); // get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:9000/api)
-router.get('/', function(request, response) {
-  response.json({ message: 'hooray! welcome to our api!' });
+router.get('/', function (request, response) {
+  response.json({message: 'hooray! welcome to our api!'});
 });
 
 // Get a list of libraries
 router.route('/libraries')
-  .get(function(request, response) {
+  .get(function (request, response) {
     if (stubArg) { // if stub enabled
       return response.json(libraries);
     }
 
     response.json([
       {
-        name : 'HTML5 Boilerplate',
-        url : 'http://www.w3.org/TR/html5/'
+        name: 'HTML5 Boilerplate',
+        url: 'http://www.w3.org/TR/html5/'
       },
       {
-        name : 'jQuery',
-        url : 'http://jquery.com/'
+        name: 'jQuery',
+        url: 'http://jquery.com/'
       },
       {
-        name : 'Backbone.js',
-        url : 'http://backbonejs.org/'
+        name: 'Backbone.js',
+        url: 'http://backbonejs.org/'
       },
       {
-        name : 'Marionette.js',
-        url : 'http://marionettejs.com/'
+        name: 'Marionette.js',
+        url: 'http://marionettejs.com/'
       }
     ]);
   });
@@ -65,12 +63,10 @@ router.route('/libraries')
 // all of our routes will be prefixed with /api
 app.use('/api', router);
 
+/*****************
+ START THE SERVER
+******************/
 
-
-//////////////////////
-// START THE SERVER //
-//////////////////////
-
-app.listen(port, function() {
+app.listen(port, function () {
   console.log('Express server listening on port %d in %s node', port, app.settings.env);
 });
