4c1873151d962af7fbf8831660a90812d37f90a6
Anthony Albertini <aalbertini@clever-age.com>
2015-03-11 17:40:58 +0100

    feat(handlebars): implements processPartialName option

--- grunt/options/handlebars.js
+++ grunt/options/handlebars.js
@@ -3,12 +3,21 @@
 module.exports = {
   compile: {
     options: {
+      namespace: 'templates',
+      amd: true,
+
       processName: function (filePath) {
         var matches = filePath.match(new RegExp('app/scripts/(modules/(\\w+)/templates|templates)/(.*).hbs'));
         return matches ? matches[3] : filePath;
       },
-      namespace: 'templates',
-      amd: true
+
+      processPartialName: function (filePath) {
+        var matches = filePath.match(new RegExp('app/scripts/(modules/(\\w+)/templates|templates)/((.*)\/?)_(.*).hbs'));
+        if (!matches) {
+          return filePath;
+        }
+        return (matches[2] ? matches[2] + '/' : '') + (matches[3] ? matches[3] : '') + matches[5];
+      }
     },
     files: {
       '<%= yeoman.tmp %>/scripts/templates.js': ['<%= yeoman.app %>/scripts/**/*.hbs']
