3d5f318a18ba00974052f1ea7a3398c021dca360
Anthony Albertini <aalbertini@clever-age.com>
2015-03-11 15:40:48 +0100

    refactor(watch): update handlebars watching

--- grunt/options/watch.js
+++ grunt/options/watch.js
@@ -5,12 +5,18 @@ module.exports = {
     files: ['<%= yeoman.app %>/styles/**/*.scss'],
     tasks: ['compass:dev']
   },
+  handlebars: {
+    files: [
+      '<%= yeoman.app %>/scripts/templates/**/*.hbs',
+      '<%= yeoman.app %>/scripts/modules/**/templates/**/*.hbs'
+    ],
+    tasks: ['handlebars']
+  },
   livereload: {
     files: [
     '<%= yeoman.app %>/*.html',
     '{.tmp,<%= yeoman.app %>}/styles/{,**/}*.css',
     '{.tmp,<%= yeoman.app %>}/scripts/{,**/}*.js',
-    '{.tmp,<%= yeoman.app %>}/scripts/{,**/}*.hbs',
     '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp}',
     // 'test/spec/{,**/}*.js' TODO livereload the next new tests
     ],
