90cbca04f72e47b940cdac91ecd042e5c8b9f468
kiki le singe <aalbertini95@gmail.com>
2015-03-30 18:25:59 +0200

    refactor(handlebars): implements partials task for handlebars

--- gulp/tasks/handlebars/handlebars.js
+++ gulp/tasks/handlebars/handlebars.js
@@ -5,7 +5,8 @@
 // https://www.npmjs.com/package/gulp-declare
 // https://www.npmjs.com/package/gulp-concat
 
-// @TODO: see https://github.com/lazd/gulp-handlebars/tree/master/examples/partials
+// Compile and register partials:
+// https://github.com/lazd/gulp-handlebars/tree/master/examples/partials
 
 var gulp = require('gulp');
 var handlebars = require('gulp-handlebars');
@@ -13,17 +14,35 @@ var wrap = require('gulp-wrap');
 var declare = require('gulp-declare');
 var concat = require('gulp-concat');
 var config = require('./config').handlebars;
+var merge = require('merge-stream');
 
-gulp.task('handlebars', function () {
+gulp.task('handlebars:dev', function () {
+  var partials = gulp.src(config.src.partials)
+    .pipe(handlebars())
+    .pipe(wrap('Handlebars.registerPartial("<%= processPartialName(file.relative) %>", Handlebars.template(<%= contents %>));', {}, {
+      imports: {
+        processPartialName: function (filePath) {
+          var matches = filePath.match(new RegExp('(modules/(\\w+)/templates|templates)/((.*)\/?)_(.*).js'));
+          if (!matches) {
+            return filePath;
+          }
+          return (matches[2] ? matches[2] + '/' : '') + (matches[3] ? matches[3] : '') + matches[5];
+        }
+      }
+    }));
+
   // Load templates from the templates/ folder relative to where gulp was executed
-  gulp.src(config.src)
+  var templates = gulp.src(config.src.templates)
     // Compile each Handlebars template source file to a template function
     .pipe(handlebars())
     .pipe(wrap('Handlebars.template(<%= contents %>)'))
     .pipe(declare({
       namespace: 'templates',
       noRedeclare: true, // Avoid duplicate declarations
-    }))
+    }));
+
+  // Output both the partials and the templates as build/js/templates.js
+  return merge(partials, templates)
     .pipe(concat('templates.js'))
     .pipe(wrap('define(["handlebars"], function(Handlebars) {<%= contents %>return this["templates"];});'))
     // Write the output into the .tmp/scripts folder
