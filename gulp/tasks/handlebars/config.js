1e9c12fc0d4e1627d5c4bcad6e9689ee568fd89a
kiki le singe <aalbertini95@gmail.com>
2015-04-12 17:07:54 +0200

    fix(config): exclude partial templates for handlebars templates

--- gulp/tasks/handlebars/config.js
+++ gulp/tasks/handlebars/config.js
@@ -3,7 +3,8 @@
 module.exports = {
   handlebars: {
     src: {
-      templates: 'app/scripts/**/*.hbs',
+      templates: ['app/scripts/**/*.hbs', '!app/scripts/**/_*.hbs'],
+      // templates: 'app/scripts/**/*.hbs',
       partials: 'app/scripts/**/_*.hbs'
     },
     dest: 'app/scripts/'
