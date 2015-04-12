4049c519aa908fa7e875294af6983877e94dd001
kiki le singe <aalbertini95@gmail.com>
2015-04-12 23:28:53 +0200

    refactor(hello): update filename

--- app/scripts/modules/hello/views/hello.js
+++ app/scripts/modules/hello/views/hello.js
@@ -7,7 +7,7 @@ function (Marionette, templates) {
   'use strict';
 
   return Marionette.ItemView.extend({
-    template: templates.hello,
+    template: templates['hello/hello'],
 
     initialize: function () {
       console.log('initialize: helloView');
