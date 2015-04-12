6ac4dbbf8ceff1f93ed3216031d18c0f56016779
kiki le singe <aalbertini95@gmail.com>
2015-04-12 23:23:15 +0200

    chore(handlebars.config): update handlebars config

--- gulp/tasks/handlebars/config.js
+++ gulp/tasks/handlebars/config.js
@@ -3,9 +3,8 @@
 module.exports = {
   handlebars: {
     src: {
-      templates: ['app/scripts/**/*.hbs', '!app/scripts/**/_*.hbs'],
-      // templates: 'app/scripts/**/*.hbs',
-      partials: 'app/scripts/**/_*.hbs'
+      templates: 'app/scripts/**/[^_]*.hbs',
+      partials: ['app/scripts/**/_*.hbs']
     },
     dest: 'app/scripts/'
     // dest: '.tmp/scripts/'
