e0d5224b37e84d927aacf2dfb7f8d395f38683a4
kiki le singe <aalbertini95@gmail.com>
2015-04-13 22:27:50 +0200

    feat(watch): Implements a watching task

---
+++ gulp/tasks/watch/config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+  watch: {
+    scss: 'app/styles/scss/**/*.scss',
+    hbs: 'app/scripts/templates/**/*.hbs',
+    hbsModule: 'app/scripts/modules/**/templates/**/*.hbs'
+  }
+};
