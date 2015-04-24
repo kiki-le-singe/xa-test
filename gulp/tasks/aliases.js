274511944ae84d2b0e3988a11006cc3dc98a4c4c
kiki le singe <aalbertini95@gmail.com>
2015-04-24 23:38:14 +0200

    feat(runSequence): implements run-sequence plugin

--- gulp/tasks/aliases.js
+++ gulp/tasks/aliases.js
@@ -1,6 +1,9 @@
 'use strict';
 
+// https://www.npmjs.com/package/run-sequence
+
 var gulp = require('gulp');
+var runSequence = require('run-sequence');
 
 gulp.task('default', ['serve']);
 
@@ -18,17 +21,20 @@ gulp.task('lint', [
   'jscs'
 ]);
 
-gulp.task('dist', [
-  'clean:dist',
-  'sass:dist',
-  'handlebars:dev',
-  'requirejs',
-  'processhtml'
-]);
+gulp.task('dist', function (cb) {
+  runSequence('clean:dist',
+    ['sass:dist', 'handlebars:dev', 'processhtml'],
+    'requirejs',
+    cb);
+});
 
-gulp.task('build', [
-  'dist',
-  'clean:release',
-  // 'compress',
-  'clean:dist'
-]);
+gulp.task('build', function (cb) {
+// gulp.task('build', ['dist'], function (cb) {
+  runSequence(
+    // 'dist',
+    'clean:release',
+    'zip',
+    'clean:dist',
+    cb
+  );
+});
