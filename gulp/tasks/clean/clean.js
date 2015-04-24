3c44f32a2a16bc2c50a97e79605996c90db9e0f6
kiki le singe <aalbertini95@gmail.com>
2015-04-24 23:35:45 +0200

    refactor(clean): update clean tasks

--- gulp/tasks/clean/clean.js
+++ gulp/tasks/clean/clean.js
@@ -10,13 +10,9 @@ var config = require('./config').clean;
 gulp.task('clean', ['clean:release', 'clean:dist']);
 
 gulp.task('clean:release', function () {
-  del(config.release, function (err, deletedFiles) {
-    console.log('Files deleted:', deletedFiles.join(', '));
-  });
+  del(config.release, {dot: true});
 });
 
 gulp.task('clean:dist', function () {
-  del(config.dist, function (err, deletedFiles) {
-    console.log('Files deleted:', deletedFiles.join(', '));
-  });
+  del(config.dist, {dot: true});
 });
