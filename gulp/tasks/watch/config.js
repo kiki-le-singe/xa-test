431f81cd22017ca1b994649237ef255cef1c2eb6
Anthony Albertini <a.albertini@useradgents.com>
2015-04-28 15:39:50 +0200

    fix(watch): fix conflict

--- gulp/tasks/watch/config.js
+++ gulp/tasks/watch/config.js
@@ -3,7 +3,6 @@
 module.exports = {
   watch: {
     scss: 'app/styles/scss/**/*.scss',
-    hbs: 'app/scripts/templates/**/*.hbs',
-    hbsModule: 'app/scripts/modules/**/templates/**/*.hbs'
+    hbs: ['app/scripts/templates/**/*.hbs', 'app/scripts/modules/**/templates/**/*.hbs']
   }
 };
