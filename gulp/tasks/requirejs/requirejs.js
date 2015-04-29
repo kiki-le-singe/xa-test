59699de2cf50544ad5ccaa5652db5d09d05dae90
kiki le singe <aalbertini95@gmail.com>
2015-04-29 16:35:26 +0200

    refactor(gulp.tasks): implements load-plugins task

--- gulp/tasks/requirejs/requirejs.js
+++ gulp/tasks/requirejs/requirejs.js
@@ -4,12 +4,11 @@
 // https://www.npmjs.com/package/gulp-uglify
 
 var gulp   = require('gulp');
-var rjs = require('gulp-requirejs');
-var uglify = require('gulp-uglify');
+var $ = require('gulp-load-plugins')();
 var config = require('./config').requirejs;
 
 gulp.task('requirejs', function () {
-  rjs(config.options)
-  .pipe(uglify())
+  $.requirejs(config.options)
+  .pipe($.uglify())
   .pipe(gulp.dest(config.dest));
 });
