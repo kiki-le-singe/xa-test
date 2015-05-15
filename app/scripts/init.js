831e67f7a08c668774f07fad06c82d27c06b877b
kiki le singe <aalbertini95@gmail.com>
2015-05-15 20:05:32 +0200

    style(init): clean up

--- app/scripts/init.js
+++ app/scripts/init.js
@@ -23,22 +23,12 @@ require.config({
     jquery: '../bower_components/jquery/dist/jquery',
     underscore: '../bower_components/lodash/lodash',
     backbone: '../bower_components/backbone/backbone',
-
-    /* alias all marionette libs */
     marionette: '../bower_components/marionette/lib/core/backbone.marionette',
     'backbone.wreqr': '../bower_components/backbone.wreqr/lib/backbone.wreqr',
     'backbone.radio': '../bower_components/backbone.radio/build/backbone.radio',
     'backbone.babysitter': '../bower_components/backbone.babysitter/lib/backbone.babysitter',
-
-    /* Alias text.js for template loading and shortcut the templates dir to tmpl */
     text: '../bower_components/requirejs-text/text',
-
-    /* Helpers */
-    helpersHBS: 'helpers/helpers',
-
-    /* requirejs-plugins */
     json: '../bower_components/requirejs-plugins/src/json',
-
     handlebars: '../bower_components/handlebars/handlebars.runtime'
   }
 });
