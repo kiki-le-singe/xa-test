0d827983fbcf0adba89e345fcba2c3ea709fbab4
kiki le singe <aalbertini95@gmail.com>
2015-05-15 20:12:18 +0200

    feat(index): init api requests

--- app/scripts/modules/books/index.js
+++ app/scripts/modules/books/index.js
@@ -1,16 +1,20 @@
 define([
   'marionette',
   '#books/controllers/nav',
-  '#books/routers/routers'
+  '#books/routers/routers',
+  '#books/requests/books'
 ],
 
-function (Marionette, NavController, Routers) {
+function (Marionette, NavController, Routers, booksApiRequests) {
   'use strict';
 
   return Marionette.Object.extend({
 
     initialize: function (options) {
       options = options || {};
+
+      // init api requests
+      booksApiRequests();
 
       this.routers = new Routers({
         controller: new NavController(options)
