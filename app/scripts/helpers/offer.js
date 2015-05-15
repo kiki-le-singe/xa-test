2a3d732499d6aef9f62d6b8e8059959a8ac61970
kiki le singe <aalbertini95@gmail.com>
2015-05-15 23:42:33 +0200

    feat(offer): refactor/add methods

--- app/scripts/helpers/offer.js
+++ app/scripts/helpers/offer.js
@@ -7,42 +7,88 @@ function (_) {
 
   var offerHelper = Object.create(null);
 
-  offerHelper.percentage = function (price, offer) {
-    if (!price && !offer) {
+  offerHelper.percentage = function (price, offers) {
+    if (!price || !offers) {
       return;
     }
 
-    var percentageValue = this.getOfferValue('percentage', offer);
-    if (!percentageValue) {
+    var value = this.getOfferValue('percentage', offers);
+    if (!value) {
       return;
     }
 
-    return price - ((percentageValue / 100) * price);
+    return price - ((value / 100) * price);
   };
 
-  offerHelper.minus = function (price, offer) {
-    if (!price && !offer) {
+  offerHelper.minus = function (price, offers) {
+    if (!price || !offers) {
       return;
     }
 
-    var minusValue = this.getOfferValue('minus', offer);
-    if (!minusValue) {
+    var value = this.getOfferValue('minus', offers);
+    if (!value) {
       return;
     }
 
-    return price - minusValue;
+    return price - value;
   };
 
-  offerHelper.getOfferValue = function (type, offer) {
-    if (!type && !offer) {
+  offerHelper.slice = function (price, offers) {
+    if (!price || !offers) {
       return;
     }
 
-    var offerType = _.pick(offer, function (value, key, object) {
+    var value = this.getOfferValue('slice', offers);
+    var sliceValue = this.getSliceValue(offers);
+    if (!value || !sliceValue) {
+      return;
+    }
+    else if (sliceValue > price) {
+      return;
+    }
+
+    var result = Math.floor(price / sliceValue) * value;
+    return price - result;
+  };
+
+  offerHelper.getSliceValue = function (offers) {
+    if (!offers) {
+      return;
+    }
+
+    var slice = this.pickOffer('slice', offers);
+    return _.pluck(slice, 'sliceValue')[0];
+  };
+
+  offerHelper.getOfferValue = function (type, offers) {
+    if (!type || !offers) {
+      return;
+    }
+
+    var offer = this.pickOffer(type, offers);
+    return _.pluck(offer, 'value')[0];
+  };
+
+  offerHelper.pickOffer = function (type, offers) {
+    if (!type || !offers) {
+      return;
+    }
+
+    return _.pick(offers, function (value, key, object) {
       return type === object[key].type;
     });
+  };
 
-    return _.pluck(offerType, 'value')[0];
+  offerHelper.getBestOffer = function (price, offers) {
+    if (!price || !offers) {
+      return;
+    }
+
+    var percentage = this.percentage(price, offers);
+    var minus = this.minus(price, offers);
+    var slice = this.slice(price, offers);
+
+    return _.min([percentage, minus, slice]);
   };
 
   return offerHelper;
